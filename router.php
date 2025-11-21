9a60aca589695e96a4ee314854ff8672a60f6001
annsshang <annsshang@gmail.com>
2025-11-21 16:28:33 -0500

    Update home.php name in router.php

--- router.php
+++ router.php
@@ -17,7 +17,7 @@ $is_admin = is_user_member_of($db, ADMIN_GROUP_ID);
 
 
 const ROUTES = array(
-  '/' => 'pages/home.php',
+  '/' => 'pages/index.php',
   '/albums' => 'pages/album.php',
   '/add' => 'pages/insertform.php',
   '/details' => 'pages/details.php',
